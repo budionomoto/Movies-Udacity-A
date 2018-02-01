bdca7f2bbdf6679d44ffe93e6859a3ccad9f4315
budionomoto <budionomotoe3@gmail.com>
2018-02-01 07:59:03 +0700

    18.02.01.Kamis.07.59

--- app/src/main/java/id/xyzsystem/budiono/moviesudacitya/cResponse.java
+++ app/src/main/java/id/xyzsystem/budiono/moviesudacitya/cResponse.java
@@ -1,8 +1,18 @@
 package id.xyzsystem.budiono.moviesudacitya;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by budiono on 30/01/18.
  */
 
 public class cResponse {
+    @SerializedName("results")
+    private List<dModel> results;
+
+    public List<dModel> getResults() {
+        return results;
+    }
 }
---
+++ app/src/main/java/id/xyzsystem/budiono/moviesudacitya/dModel.java
@@ -0,0 +1,44 @@
+package id.xyzsystem.budiono.moviesudacitya;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by budiono on 01/02/18.
+ */
+
+public class dModel {
+    @SerializedName("overview")
+    private String overview;
+
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    @SerializedName("release_date")
+    private String releaseDate;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("vote_average")
+    private double voteAverage;
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+}
